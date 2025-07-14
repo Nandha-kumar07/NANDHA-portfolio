d32df6c29a4659c12dba1bb6164c196891871eec
nan <nandhakumar.peee2023@sece.ac.in>
2025-07-14 18:47:09 +0530

    Add base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { ghPages } from 'vite-plugin-gh-pages'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    hmr: {
-      overlay: false
-    }
-  }
+  plugins: [react(), ghPages()],
+  base: '/NANDHA-portfolio/'
+,  // Replace with your GitHub repo name
 })
